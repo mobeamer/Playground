bf03f22fa580fd52eedeb1a17b1e6124092b6777
Markus Beamer <MBeamer@avidxchange.com>
2019-04-12 17:22:10 -0400

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World, I am alive!");
+    response.end("Hello World, Look at me now!");
 });
 
 const port = process.env.PORT || 1337;
